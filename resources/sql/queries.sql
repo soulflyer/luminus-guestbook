56ee960f5d4e56cb6a3fb607830b40f9fe2369ed
iain <iain@soulflyer.co.uk>
2017-06-13 12:18:07 +0700

    Display db contents on home page

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,8 +1,8 @@
 -- :name save-message! :! :n
--- creates a new message
+-- :doc creates a new message
 INSERT INTO guestbook
 (name, message, timestamp)
 VALUES (:name, :message, :timestamp)
 -- :name get-messages :? :*
--- selects all available messages
+-- :doc selects all available messages
 SELECT * from guestbook
--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,17 +1,19 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [guestbook.db.core :as db]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    {:messages (db/get-messages)}))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/message" request (db/save-message! request))
   (GET "/about" [] (about-page)))
-
